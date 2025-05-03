1023f0993cf05575eceeff6940b0785a75d331af
DarQxxx <norbertju@gmail.com>
2025-05-03 16:00:12 +0200

    add test check for be connection

--- criti-cloud-front/src/App.tsx
+++ criti-cloud-front/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [response, setResponse] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/test") // <- dopasuj endpoint
+      .then((res) => {
+        if (!res.ok) throw new Error(`Status: ${res.status}`);
+        return res.json();
+      })
+      .then((data) => setResponse(data))
+      .catch((err) => setResponse(err.message));
+  }, []);
+
+  console.log(response);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <pre>
+        {response
+          ? JSON.stringify(response, null, 2)
+          : "Czekam na odpowiedź..."}
+      </pre>
+    </div>
+  );
 }
 
-export default App
+export default App;
