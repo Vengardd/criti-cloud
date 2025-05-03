c0b61bedb1c58c31c2a720ceea2539a3ce1b39f2
DarQxxx <norbertju@gmail.com>
2025-05-03 17:13:10 +0200

    CC-1 [Frontend] fetch string value from backed

--- criti-cloud-front/src/App.tsx
+++ criti-cloud-front/src/App.tsx
@@ -1,27 +1,20 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [response, setResponse] = useState(null);
+  const [response, setResponse] = useState<string>("");
 
   useEffect(() => {
-    fetch("http://localhost:8080/test") // <- dopasuj endpoint
-      .then((res) => {
-        if (!res.ok) throw new Error(`Status: ${res.status}`);
-        return res.json();
-      })
-      .then((data) => setResponse(data))
-      .catch((err) => setResponse(err.message));
+    fetch("http://localhost:8080/test")
+      .then((res) => res.text())
+      .then((text) => setResponse(text))
+      .catch((err) => console.error(err));
   }, []);
 
   console.log(response);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <pre>
-        {response
-          ? JSON.stringify(response, null, 2)
-          : "Czekam na odpowiedź..."}
-      </pre>
+      <pre>{response ? response : "Czekam na odpowiedź..."}</pre>
     </div>
   );
 }
