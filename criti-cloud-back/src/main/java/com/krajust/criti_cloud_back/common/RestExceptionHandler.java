dd16a55e3a28488b9e5574abb39203a7013ae9ef
Cezary Krawczyk <cezary.k.krawczyk@gmail.com>
2026-01-04 13:56:00 +0100

    CC-30 [backend] Add graalVM build

--- criti-cloud-back/src/main/java/com/krajust/criti_cloud_back/common/RestExceptionHandler.java
+++ criti-cloud-back/src/main/java/com/krajust/criti_cloud_back/common/RestExceptionHandler.java
@@ -1,12 +1,15 @@
 package com.krajust.criti_cloud_back.common;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
@@ -21,6 +24,14 @@ public class RestExceptionHandler {
         return e.getMessage();
     }
 
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<Void> handleNoResourceFound(NoResourceFoundException ex, HttpServletRequest req) {
+        if (req.getRequestURI().equals("/favicon.ico")) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 
 
 }
